2b4510b7f60718e5c7a0ca47450d209d6a8d4076
AlexMkk <asmkk@mail.ru>
2021-03-24 21:31:05 +0300

    fix resolvers

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -2,27 +2,23 @@ const User = require('../models/User')
 
 const resolvers = {
   Query: {
-    user: async (_, { id }) => {
-      return await User.findById(id)
+    user: (_, { id }) => {
+      return User.findById(id)
     },
-    users: async () => await User.find()
+    users: () => User.find()
   },
   Mutation: {
-    createUser: async (_, args) => {
-      const user = new User({ name: args.input.name, email: args.input.email })
-      return await user.save()
+    createUser: (_, { input }) => {
+      return User.create(input)
     },
-    updateUser: async (_, args) => {
-      const { id, input } = args,
-        user = await User.findById(id)
-      const toChange = {
-        name: input.name, email: input.email
-      }
-      Object.assign(user, toChange)
-      return await user.save()
+    updateUser: (_, { id, input }) => {
+      return User.findByIdAndUpdate(id, input, {
+        runValidators: true,
+        new: true
+      })
     },
-    deleteUser: async (_, { id }) => {
-      await User.deleteOne({ "_id": id })
+    deleteUser: (_, { id }) => {
+      User.findOneAndDelete(id)
     }
   }
 }
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import { useQuery, useMutation } from 'react-apollo'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
@@ -6,99 +7,109 @@ import TableContainer from '@material-ui/core/TableContainer'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
-import { Button } from '@material-ui/core'
+import { Button, Snackbar } from '@material-ui/core'
 import LinearIndeterminate from './../UI/Loader'
 import { useHistory } from 'react-router-dom'
 import { GET_USERS } from '../../query/users'
 import { useStyleTableUsers } from './../UI/Styles'
 import { DELETE_USER } from './../../mutations/users'
+import { Alert } from '../UI/Alert'
 
 const Users = () => {
-  const classes = useStyleTableUsers()
-  const history = useHistory()
-  
-  const { loading, error, data, refetch } = useQuery(GET_USERS,{
+  const classes = useStyleTableUsers(),
+    history = useHistory()
+  const [open, setOpen] = useState(false)
+
+  const showAlert = () => {
+    setOpen(true)
+  }
+  const hideAlert = (event, reason) => {
+    if (reason === 'clickaway') return
+    setOpen(false)
+  }
+  const { loading, error, data, refetch } = useQuery(GET_USERS, {
     onCompleted: () => {
       if (history.location.state) {
-        console.log('делаем refetch', history.location.state )
         refetch()
       }
     }
   })
-  
+
   const [deleteUser] = useMutation(DELETE_USER)
 
   const onDelete = id => {
     deleteUser({ variables: { id } })
-        .then(() => 
-          refetch()
-          // showAlert()
+      .then(() =>
+        refetch(),
+        showAlert()
       )
   }
 
-  
-  
-
-  if (loading) return <LinearIndeterminate/>
+  if (loading) return <LinearIndeterminate />
   if (error) return <p>Error :(</p>
-  
+
   return (
-  <Paper>
-    <TableContainer>
-      <Table className={classes.table} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">ID</TableCell>
-            <TableCell>Имя</TableCell>
-            <TableCell colSpan={4}>Email</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.users.map((user) => (
-            <TableRow key={user.id}>
-              <TableCell align="center" component="th" scope="row">
-                {user.id}
-              </TableCell>
-              <TableCell>{user.name}</TableCell>
-              <TableCell>{user.email}</TableCell>
-              <TableCell style={{ width: 90 }}>
-                <Button
-                  variant="contained"
-                  size="small"
-                  onClick={() => history.push(`/${user.id}`)}
-                >Смотреть</Button>
-              </TableCell>
-              <TableCell style={{ width: 110 }}>
-                <Button
-                  variant="contained"
-                  size="small"
-                  color="primary"
-                  onClick={() => history.push(`/${user.id}/edit`)}
-                >Редактировать</Button>
-              </TableCell>
-              <TableCell style={{ width: 110 }}>
-                <Button
-                  variant="contained"
-                  size="small"
-                  color="secondary"
-                  onClick={() => onDelete(user.id)}
-                >Удалить</Button>
-              </TableCell>
+    <Paper>
+      <TableContainer>
+        <Table className={classes.table} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">ID</TableCell>
+              <TableCell>Имя</TableCell>
+              <TableCell colSpan={4}>Email</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  
-    <Button
-      variant="contained"
-      color="primary"
-      className={classes.addButton}
-      onClick={() => history.push(`/add`)}
-    >Добавить
+          </TableHead>
+          <TableBody>
+            {data.users.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell align="center" component="th" scope="row">
+                  {user.id}
+                </TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell style={{ width: 90 }}>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    onClick={() => history.push(`/${user.id}`)}
+                  >Смотреть</Button>
+                </TableCell>
+                <TableCell style={{ width: 110 }}>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    color="primary"
+                    onClick={() => history.push(`/${user.id}/edit`)}
+                  >Редактировать</Button>
+                </TableCell>
+                <TableCell style={{ width: 110 }}>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    color="secondary"
+                    onClick={() => onDelete(user.id)}
+                  >Удалить</Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Button
+        variant="contained"
+        color="primary"
+        className={classes.addButton}
+        onClick={() => history.push(`/add`)}
+      >Добавить
     </Button>
-  </Paper>
-  
+      <Snackbar open={open} autoHideDuration={3000} onClose={hideAlert}>
+        <Alert onClose={hideAlert} severity="success">
+          Пользователь удален!
+      </Alert>
+      </Snackbar>
+    </Paper>
+
   )
 }
 export default Users
