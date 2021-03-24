53b7f31839f36de45ccdd12f42d3db1d425cb3c9
AlexMkk <asmkk@mail.ru>
2021-03-24 20:42:25 +0300

    fix

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -2,29 +2,27 @@ const User = require('../models/User')
 
 const resolvers = {
   Query: {
-    user: async (_, {id}) => {
+    user: async (_, { id }) => {
       return await User.findById(id)
     },
     users: async () => await User.find()
   },
   Mutation: {
     createUser: async (_, args) => {
-      console.log('создание', args)
       const user = new User({ name: args.input.name, email: args.input.email })
       return await user.save()
     },
     updateUser: async (_, args) => {
-      console.log('обновление', args)
-      const {id, input} = args,
-            user = await User.findById(id)
+      const { id, input } = args,
+        user = await User.findById(id)
       const toChange = {
         name: input.name, email: input.email
       }
       Object.assign(user, toChange)
       return await user.save()
     },
-    deleteUser: async (_, {id}) => {
-      await User.deleteOne({"_id": id})
+    deleteUser: async (_, { id }) => {
+      await User.deleteOne({ "_id": id })
     }
   }
 }
